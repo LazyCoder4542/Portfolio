df185022e719d279cdbb08ec42191eb09d1c80c6
Adeola Fadare <adeolafadare8@gmail.com>
2022-09-04 15:50:21 +0100

    Completed intro section

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-$(document).ready(function() {
-    setInterval(() => {
-       $('p').slideToggle('300')
-    }, 100);
+$(document).ready(function () {
+    /* ICONS SLIDE */
+    var time = 25000
+    $('.icons-slider .icons').css('animationDuration', `${time}ms`);
+    var icons = $('.icons-slider .icons').get(0)
+    var iconsClone = icons.cloneNode('true')
+    $(iconsClone).addClass('copy');
+    $(icons).parent().append(iconsClone);
+    $(icons).css({ 'animationName': 'slide' })
+    $(iconsClone).css({ 'animationName': 'slide', 'animationDelay': `${time / 2}ms` })
+
 }
 );
