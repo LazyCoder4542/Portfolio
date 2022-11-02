eca5a29615fc2c5933c0e6875b96259d08004347
Lazy Coder <adeolafadare8@gmail.com>
2022-11-02 20:58:36 +0100

    added more sections

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -6,6 +6,8 @@ import { ReactComponent as Logo} from "./assets/icons/logo.svg";
 import { ReactComponent as WaveSVG} from "./assets/icons/waves.svg";
 import { ReactComponent as HatIcon} from "./assets/icons/hat.svg";
 import { ReactComponent as BagIcon} from "./assets/icons/bag.svg";
+import { ReactComponent as DevIcon} from "./assets/icons/dev.svg";
+import { ReactComponent as PhoneIcon} from "./assets/icons/phone.svg";
 import { ReactComponent as HTMLIcon} from "./assets/icons/html.svg";
 import { ReactComponent as CSSIcon} from "./assets/icons/css.svg";
 import { ReactComponent as JSIcon} from "./assets/icons/js.svg";
@@ -206,6 +208,33 @@ function App() {
                   <span><BootstrapIcon /></span>
                 </div>
               </div>
+            </div>
+          </section>
+          <section id='about' data-scroll-section>
+            <header>
+              <h2>
+                <span>About</span>
+                <span>
+                  <DevIcon />
+                </span>
+              </h2>
+            </header>
+            <div className="wrapper">
+              <p>My name is Adeola Fadare, and I’m a Front-end Developer. I love creating amazing projects with seamless expeience. I also enjoy working with people to create amazing products. I am currently freelancing at Byte tech services.</p>
+              <p>I am currently based in Lagos, Nigeria.</p>
+            </div>
+          </section>
+          <section id='contact' data-scroll-section>
+            <header>
+              <h2>
+                <span>Contact</span>
+                <span>
+                  <PhoneIcon />
+                </span>
+              </h2>
+            </header>
+            <div className="wrapper">
+              <p>If you’ll like to know  more about my work, rub minds together on a project or collaborate with you on a project , feel free to shoot me an email-fadareadeola@gmail.com</p>
             </div>
           </section>
           <section data-scroll-section>
