9b84e8e803d74f7d515e454033be2540aa5153c1
Lazy Coder <adeolafadare8@gmail.com>
2023-01-11 08:00:42 +0100

    deleted test file

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './locomotive-scroll.css';
 import App from './App';
-import Test from './Test';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
